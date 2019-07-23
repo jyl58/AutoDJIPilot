7ba4f91297e6c8a80da269c9a0917d5b06fe93c3
jyl58 <jiyingliang369@126.com>
2019-07-23 08:42:37 +0800

    修改自启动脚本

--- AutoRun.sh
+++
@@ -1,8 +0,0 @@
-#! bin/bash
-cd /root/AutoDJIPilot/build/AutoPilotDJI
-
-screen -d -m -s ./AutoPilotDJI /root/AutoDJIPilot/config/config.lua
-
-exit 0
-
-
---
+++ auto_run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+screen -d -m -s /bin/bash /root/AutoDJIPilot/build/AutoPilot/AutoPilotDJI /root/AutoDJIPilot/config/config.lua
+
+exit 0
+
+
