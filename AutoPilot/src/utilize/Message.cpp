6cf6686eb1ee1021023326c85be74f5bfa030fa5
jyl58 <jiyingliang369@126.com>
2019-08-01 15:15:09 +0800

    修改提示信息格式，隐藏文件路径，只保留文件名用于定位提示信息

--- AutoPilot/src/utilize/Message.cpp
+++ AutoPilot/src/utilize/Message.cpp
@@ -1,6 +1,7 @@
 #include "Message.h"
-void DWAR(std::string warn_file,int line ,std::string msg,int fd){														
-	std::string warn_message="[WAR]"+warn_file+": "+std::to_string(line)+": "+ msg;
+void DWAR(std::string warn_file,int line ,std::string msg,int fd){
+														
+	std::string warn_message="[WAR]"+warn_file.substr(warn_file.find_last_of("/"))+": "+std::to_string(line)+": "+ msg;
 	NOTICE_MSG(fd,warn_message);
 	FlightLog::writeLogBuffer(warn_message);																
 }
